c2e28c09ea8d968b4d5fcda822a390751149d158
joangnz <joangnz@gmail.com>
2024-11-13 17:48:42 +0100

    added table for users in admin.php, new user in users.json

--- sites/admin.php
+++ sites/admin.php
@@ -7,6 +7,47 @@ if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") {
     header("location: main.php");
 }
 
+function readUsers()
+{
+    $users = json_decode(file_get_contents("../data/users.json"), true) ?? [];
+    return $users;
+}
+
+function updateUser($id, $newName, $newEmail)
+{
+    $users = readUsers();
+
+    $userIndex = array_search($id, array_column($users, 'id'));
+    if ($userIndex !== false) {
+        $users[$userIndex]['name'] = $newName;
+        $users[$userIndex]['email'] = $newEmail;
+
+        file_put_contents('../data/users.json', json_encode($users, JSON_PRETTY_PRINT));
+
+        return "User updated";
+    }
+
+    return "User not found";
+
+}
+
+function deleteUser($id)
+{
+    $users = readUsers();
+
+    $userIndex = array_search($id, array_column($users, "id"));
+    if ($userIndex !== false) {
+        unset($users[$userIndex]);
+        $users = array_values($users);
+
+        file_put_contents('../data/users.json', json_encode($users, JSON_PRETTY_PRINT));
+
+        return "User deleted";
+    }
+
+    return "User not found";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +71,30 @@ if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") {
 <body>
     <header id="header"></header>
     <main id="main" class="flex">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Role</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $users = readUsers();
+                foreach ($users as $user): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($user['id']) ?></td>
+                        <td><?php echo htmlspecialchars($user['user']) ?></td>
+                        <td><?php echo htmlspecialchars($user['role']) ?></td>
+                        <td class="actions">
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
     </main>
     <footer id="footer"></footer>
 </body>
